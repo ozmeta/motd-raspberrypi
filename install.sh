3ca53746bdc001e48da07f8e47d13f7f1b75dd05
ozmeta <141689708+ozmeta@users.noreply.github.com>
2025-02-22 19:13:34 +0300

    Remove legacy MOTD scripts and add .gitignore for temporary files - Simplified configuration

--- install.sh
+++ install.sh
@@ -3,41 +3,39 @@
 # Check if the script is run as root
 if [ "$EUID" -ne 0 ]; then
     echo "Please run as root"
-    return 1
+    exit 1
 fi
 
 # Define the source and destination paths
-MOTD_CONFIG="/etc/motd-config"
-UPDATE_MOTD="/usr/bin/update-motd"
-UPDATE_MOTD_D="/etc/update-motd.d/"
+SOURCE_FILE="./motd-raspberrypi"
+DEST_DIR="/etc/update-motd.d"
+DEST_FILE="$DEST_DIR/motd-raspberrypi"
 
 # Remove any existing files to prevent confusion
-echo "Removing existing "$MOTD_CONFIG"..."
-rm -f "$MOTD_CONFIG"
+echo "Removing existing MOTD..."
+if rm -rf "$DEST_DIR"/*; then
+    echo "Existing MOTD removed."
+else
+    echo "Failed to remove existing MOTD."
+    exit 1
+fi
 
-echo "Removing existing "$UPDATE_MOTD"..."
-rm -f "$UPDATE_MOTD"
+# Copy the new MOTD
+echo "Copying new scripts to $DEST_DIR..."
+if cp "$SOURCE_FILE" "$DEST_FILE"; then
+    echo "Scripts copied successfully."
+else
+    echo "Failed to copy scripts."
+    exit 1
+fi
 
-echo "Removing existing "$UPDATE_MOTD_D" scripts..."
-rm -rf "$UPDATE_MOTD_D"*
-
-# Copy the motd-config file
-echo "Copying to $MOTD_CONFIG..."
-cp ./"$MOTD_CONFIG" "$MOTD_CONFIG"
-chmod 644 "$MOTD_CONFIG"
-
-# Copy the update-motd script
-echo "Copying to $UPDATE_MOTD..."
-cp ./"$UPDATE_MOTD" "$UPDATE_MOTD"
-chmod 755 "$UPDATE_MOTD"
-
-# Copy the update-motd.d scripts
-echo "Copying to $UPDATE_MOTD_D..."
-cp -r ./"$UPDATE_MOTD_D"* "$UPDATE_MOTD_D"
-chmod 755 "$UPDATE_MOTD_D"*
-
-# Run update-motd to compile for the first time with default settings
-echo "Running update-motd for the first time..."
-sudo /usr/bin/update-motd
+# Set the appropriate permissions
+echo "Setting permissions for $DEST_FILE..."
+if chmod 755 "$DEST_FILE"; then
+    echo "Permissions set successfully."
+else
+    echo "Failed to set permissions."
+    exit 1
+fi
 
 echo "Installation complete."
